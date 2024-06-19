f44966d8a3e7484ab2bba116f35857a0c9c283a5
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-06-18 23:54:02 -0300

    perfil 2

--- src/utils/api/fotoPerfil.js
+++ src/utils/api/fotoPerfil.js
@@ -1,8 +1,8 @@
-export const handleSoporte = async (formdata) => {
+export const fotoPerfil = async (formdata) => {
     const token = localStorage.getItem("userToken")
     if (!token) return
 
-    const response = await fetch("http://localhost:3500/api/soporte", {
+    const response = await fetch("http://localhost:3500/api/foto-perfil", {
         method: "POST",
         mode: "cors",
         headers: {"Authorization":`Bearer ${token}`},
@@ -11,5 +11,5 @@ export const handleSoporte = async (formdata) => {
 
     if (response.status == 204) return 204;
 
-    return true
+    return response.json()
 }
